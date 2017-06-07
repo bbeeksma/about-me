feb6e389429702569c93a95652911599cafaf061
Meepedy <david@psyon.org>
2017-06-06 20:51:42 -0500

    Added questions and forms for answers

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Loaded app.js");
+console.log('Loaded app.js');
+function submit(){
+  var answers = {
+    name: document.getElementById('name').value,
+    color: document.getElementById('color').value,
+    food: document.getElementById('food').value,
+    language: document.getElementById('language').value,
+    passion: document.getElementById('passion').value
+  };
+  console.log(answers);
+}
